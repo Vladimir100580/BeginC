115af16a574294251b747c4cf5b26e619070e675
Vladimir Poboykin <vovar3573@mail.ru>
2022-12-13 21:09:26 +0300

    adding Masha

--- Examples/Example004/Program.cs
+++ Examples/Example004/Program.cs
@@ -1,5 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
-double numberA = 12;
-int numberВ = 5;
-double result = numberA / numberВ;
-Console.WriteLine(result);
+double numberA = new Random().Next(1,10);
+double numberB = new Random().Next(1,10);
+double result = numberA / numberB;
+Console.WriteLine(numberA, result);
+
---
+++ Examples/Example005/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+if (username.ToLower() == "маша")
+{
+    Console.Write("Ничего себе, вот она - Маша!!");
+}
+else
+{
+    Console.Write("Hello, ");
+    Console.Write(username);
+}
