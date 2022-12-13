29054fe06582f1d8508e526c079b7caa27a84f28
Vladimir Poboykin <vovar3573@mail.ru>
2022-12-13 20:37:09 +0300

    adding div

---
+++ Examples/Example004/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+int numberA = 12;
+int numberВ = 5;
+double result = numberA / numberВ;
+Console.WriteLine(result);
---
+++ Examples/Examples003/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+int numberA = 31;
+int numberВ = 5;
+int result = numberA + numberВ;
+Console.WriteLine(result);
+
